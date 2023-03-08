dfc745d620c2e669e5ff3be41283898d9c75a48f
AnastasiaE3 <125825396+AnastasiaE3@users.noreply.github.com>
2023-03-07 21:19:39 -0500

    made changes

--- src/test/java/LogginLabTest.java
+++ src/test/java/LogginLabTest.java
@@ -31,4 +31,25 @@ public class LogginLabTest {
             }
         }
     }
+
+    @org.junit.Test
+    public void thresholdReached() {
+        Integer finalLimit = 5;
+
+        LogginLab lab = new LogginLab();
+        lab.thresholdReached(finalLimit);
+
+        for (Integer i = 1; i <= finalLimit; i ++) {
+            if (lab.thresholdReached(i)) {
+                logger.log(Level.INFO, "Threshold not reached! It is "+i);
+                assertTrue(lab.thresholdReached(i));
+            } else {
+                logger.log(Level.INFO, "Threshold finally reached!");
+                assertFalse(lab.thresholdReached(i));
+            }
+        }
+    }
+
+
+
 }
